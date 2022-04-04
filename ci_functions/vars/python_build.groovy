20fe9882aaafe97a4c27d4a9dadd9937cc8560e4
CherylKong19 <72482671+CherylKong19@users.noreply.github.com>
2022-04-03 19:26:21 -0700

    Update python_build.groovy

--- ci_functions/vars/python_build.groovy
+++ ci_functions/vars/python_build.groovy
@@ -1,13 +1,24 @@
 def call (repo) {
     pipeline {
         agent any
-//         parameters {
-//         booleanParam(defaultValue: false, description:'Deploy the App', name: 'DEPLOY')
-//         }
         stages {
             stage('Build') {
                 steps {
                     sh "pip install -r requirements.txt"
+                }
+            }
+            stage('Python Lint') {
+                steps {
+                    sh "pylint-fail-under --fail_under 5.0  *.py "
+                }
+            }
+            stage('Package') {
+                steps {
+                    withCredentials([string(credentialsId: 'DockerHub', variable: 'TOKEN')]) {
+                    sh "docker login -u cherylk19 -p '$TOKEN' docker.io"
+                    sh "docker build -t cherylk19/${repo}:latest ."
+                    sh "docker push cherylk19/${repo}:latest"
+                    }
                 }
             }
             stage('Check Container running') {
@@ -18,33 +29,12 @@ def call (repo) {
                     }
                 }
             }
-            stage('Python Lint') {
-                steps {
-                    sh "pylint-fail-under --fail_under 5.0  *.py "
-                }
-            }
-            stage('Package') {
-//                 when {
-//                      expression { env.GIT_BRANCH == 'main' }
-//                     branch 'main'
-//                 }
-                steps {
-                    withCredentials([string(credentialsId: 'DockerHub', variable: 'TOKEN')]) {
-                    sh "docker login -u cherylk19 -p '$TOKEN' docker.io"
-                    sh "docker build -t cherylk19/${repo}:latest ."
-                    sh "docker push cherylk19/${repo}:latest"
-                    }
-                }
-            }
             stage('Deploy') {
-//                 when {
-//                     expression { params.DEPLOY }
-//                 }
                 steps {
                     sshagent (credentials: ['cheryl-vm']) {
                         withCredentials([string(credentialsId: 'DockerHub', variable: 'TOKEN')]) {
                             sh "ssh -o StrictHostKeyChecking=no azureuser@acit3855-kafka.eastus.cloudapp.azure.com 'docker login -u cherylk19 -p '$TOKEN' docker.io && \
-                            cd ~/lab8/deployment && docker-compose stop ${repo} && docker container prune -f && docker pull cherylk19/${repo}:latest && docker-compose up -d'"
+                            cd ~/lab8/deployment && docker pull cherylk19/${repo}:latest && docker-compose up -d'"
                         }
                     }
                 }
